ea315f4987563a3cfa7ca259c3b212ee65092be6
kishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2025-08-06 00:39:31 +0530

    first commit

---
+++ src/main/java/com/wysper/WysperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wysper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WysperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WysperBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wysper/WysperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wysper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WysperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
